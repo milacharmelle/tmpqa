0a01f1fe2890512d6421bf8135254567fe8c4d14
Your Name <102684272+milacharmelle@users.noreply.github.com>
2022-07-06 22:57:42 +0500

    C:\Users\Admin\IdeaProjects\tmpqa\src

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         int water = 3000; // миллилитров воды
         int potatoes = 5; // картофелин
         int chicken = 6; // куриных бёдер
-        int spices = 10  ; // ложек специй
+        int spices = 10; // ложек специй
 
         System.out.println("Сварили суп. На одного человека вышло:");
         System.out.println((water / eaters) + " миллилитров воды");
